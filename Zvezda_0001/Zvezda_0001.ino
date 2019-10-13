68a4eb9e43fa413548d172628cfe96aaff742c1c
egalexey <34672972+egalexey@users.noreply.github.com>
2019-10-13 19:14:15 +0300

    Update Zvezda_0001.ino

--- Zvezda_0001/Zvezda_0001.ino
+++ Zvezda_0001/Zvezda_0001.ino
@@ -46,11 +46,26 @@ int colorBlack = 100;
 int speedTest  = 20;
 int speedTestB = -100;
 
+enum State_enum {STOP, FORWARD, ROTATE_RIGHT, ROTATE_LEFT, MOVE_BACK};
+enum Sensors_enum {NONE, SENSOR_RIGHT, SENSOR_LEFT, BOTH};
+
+void state_machine_run(uint8_t sensors);
+void motors_stop();
+void motors_forward();
+void motors_right();
+void motors_left();
+void motors_back();
+uint8_t read_IR();
+ 
+uint8_t state   = STOP;
+uint8_t Sensors = NONE;
+
+
 
 void setup() {
   ////// Led Mix Of Beginning \\\\\\
 
-  Zvezda.MixLed(4);
+  Zvezda.MixLed(2);
 
   ////// Object Sensors \\\\\\
 
@@ -89,30 +104,141 @@ void setup() {
 
 void loop() {
 
-  if (digitalRead(StartPin) == LOW ) {
-    digitalWrite(Led2, HIGH);
-
-    Zvezda.Mooving(speedTest, speedTest);
-
-    if(analogRead(LQtr) < colorBlack && analogRead(RQtr) > colorBlack) {
+  if (digitalRead(StartPin) == LOW ) 
+     {
+       digitalWrite(Led2, HIGH);
+       
+       //Serial.println(Sensors);
+       
+       Sensors = read_IR();
+       
+       
+       state_machine_run(Sensors);
+       delay(5);
+       } 
+  else 
+     {
+      digitalWrite(Led2, LOW);
       Zvezda.Mooving(0, 0);
       delay(10);
-      Zvezda.Mooving(speedTestB, speedTestB);
-      delay(300);
-      Zvezda.TurnRight(100, 400);
+     }
+}
+
+void state_machine_run(uint8_t sensors) 
+{
+  switch(state)
+  {
+    case STOP:
+      if(sensors == NONE){
+        motors_forward();
+        state = FORWARD;
+      }
+      else if(sensors == SENSOR_RIGHT){
+        motors_left();
+        state = ROTATE_LEFT;
+      }
+      else if(sensors == SENSOR_LEFT){
+        motors_right();
+        state = ROTATE_RIGHT;
+      }
+      else{
+        motors_back();
+        state = MOVE_BACK;
+      }
+      break;
+       
+    case FORWARD:
+      if(sensors != NONE){
+        motors_stop();
+        state = STOP;
+      }
+      break;
+ 
+    case ROTATE_RIGHT:
+      if(sensors == NONE || sensors == SENSOR_RIGHT){
+        motors_stop();
+        state = STOP;
+      }
+      break;
+ 
+    case ROTATE_LEFT:
+      if(sensors != SENSOR_RIGHT)
+      {
+        motors_stop();
+        state = STOP; 
+      }
+      break;
+    case MOVE_BACK:{
+        motors_stop();
+        state = STOP; 
+     } 
+     break;
+  }
+}
+ 
+void motors_stop()
+{
+  Zvezda.InertialMooving(0, 0);
+  delay(10);
+}
+ 
+void motors_forward()
+{
+  Zvezda.InertialMooving(speedTest, speedTest);
+  //Zvezda.Mooving(speedTest, speedTest);
+}
+
+void motors_back()
+{
+  Zvezda.InertialMooving(-speedTest, -speedTest);
+  delay(100);
+  //Zvezda.Mooving(-20, 20);
+  Zvezda.InertialMooving(-20, 20);
+  delay(10);
+}
+ 
+void motors_right()
+{
+  //Zvezda.Mooving(-20, 20);
+  Zvezda.InertialMooving(-20, 20);
+  delay(10);
+}
+ 
+void motors_left()
+{
+  //Zvezda.Mooving(20, -20);
+  Zvezda.InertialMooving(20, -20);
+  delay(10);
+}
+ 
+uint8_t read_IR()
+{
+ uint8_t sensors = NONE;
+   if(analogRead(LQtr) < colorBlack && analogRead(RQtr) > colorBlack) {
+     
+     //Serial.println("left");
+     
+     return SENSOR_LEFT;  
     }
     if (analogRead(LQtr) > colorBlack && analogRead(RQtr) < colorBlack) {
-      Zvezda.Mooving(0, 0);
-      delay(10);
-      Zvezda.Mooving(speedTestB, speedTestB);
-      delay(300);
-      Zvezda.TurnLeft(100, 400);
+      
+      //Serial.println("RIGHT");
+      
+      return SENSOR_RIGHT;
     }
-
-
-  } else {
-    digitalWrite(Led2, LOW);
-    Zvezda.Mooving(0, 0);
-    delay(1000);
-  }
+    if(analogRead(LQtr < colorBlack) && analogRead(RQtr) < colorBlack) {
+      
+      //speedTest = -20; 
+      //Serial.println("BOTH");
+      
+      return BOTH;      
+      }
+    if(analogRead(LQtr > colorBlack) && analogRead(RQtr) > colorBlack) {
+      //speedTest = 20;
+     
+      //Serial.println("NONE");
+      
+      return NONE;           
+      } 
+  return sensors;
 }
